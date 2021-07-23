158e326d3c1d1b9bef766c8aeec90a223e15fd74
wbabello <wbabello@yahoo.com>
2021-07-23 11:21:59 +0100

    saved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MovieList from "./MoviesList";
+import AddToFavourites from "./AddToFavourites";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+  };
+
   return (
-    <div className="App">
+    <div className="Container ">
       <h1> Top Movies </h1>
       <MovieList></MovieList>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="row">
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddToFavourites}
+        />
+      </div>
+      <div className="container-fluid movie-app">
+        <div className="row d-flex align-items-center mt-4 mb-4"></div>
+        <div className="row">
+          <MovieList
+            movies={movies}
+            handleAddFavouritesClick={addFavouriteMovie}
+            favouriteComponent={AddToFavourites}
+          />
+        </div>
+        {/* <div className="row d-flex align-items-center mt-4 mb-4">
+          <MovieListHeading heading="Favourites" />
+        </div> */}
+        <div className="row">
+          <MovieList
+            movies={favourites}
+            // handleFavouritesClick={removeFavouriteMovie}
+            // favouriteComponent={RemoveFavourites}
+          />
+        </div>
+      </div>
+      );
     </div>
   );
 }
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import React from "react";
 
 function MovieList() {
+  const MovieList = (props) => {
+    const FavouriteComponent = props.favouriteComponent;
+  };
+
   const [movieList, setMovieList] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const getMovieList = async () => {
     const response = await axios.get("https://hub.dummyapis.com/vj/wzGUkpZ");
@@ -18,15 +24,25 @@ function MovieList() {
       <h2>Top Recommendation </h2>
       <ul>
         {movieList.map((movieItem) => (
-          <li key={`Movie ${movieList.name}`}>
-            {movieItem.name}, {`Released On ${movieItem.releasedOn}`},
+          <div className="movieList-Container" key={`Movie ${movieList.name}`}>
             <img
               src={movieItem.bannerUrl}
               className="Movie-logo"
               alt="movie"
             ></img>
-            {`Movie Watched: ${movieItem.watched.boolValue ? "true" : "false"}`}
-          </li>
+            <div className="Favourite-button">Add to Favourites</div>
+
+            <div className="overlay d-flex align-items-center justify-content-center">
+              {/* <FavouriteComponent /> */}
+            </div>
+
+            <div className="movieList-details" key={`Movie ${movieList.name}`}>
+              {movieItem.name}, {`Released On ${movieItem.releasedOn}`},
+              {`Movie Watched: ${
+                movieItem.watched.boolValue ? "true" : "false"
+              }`}
+            </div>
+          </div>
         ))}
       </ul>
     </>
